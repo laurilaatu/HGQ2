7c827edbd4cedb5bcf456f691651654fbcc49054
Chang Sun <chsun@cern.ch>
2024-09-04 15:56:29 -0700

    chore(layer): import all layers in ops

--- qkeras_next/layers/__init__.py
+++ qkeras_next/layers/__init__.py
@@ -3,4 +3,5 @@ from .batch_normalization import QBatchNormalization
 from .conv import QConv1D, QConv2D, QConv3D
 from .core import *
 from .einsum_dense_batchnorm import QEinsumDenseBatchnorm
+from .ops import *
 from .softmax import QSoftmax
--- qkeras_next/layers/ops/__init__.py
+++ qkeras_next/layers/ops/__init__.py
@@ -1,2 +1,16 @@
 from .accum import QMeanPow2, QSum
 from .merge import QAdd, QAveragePow2, QDot, QEinsum, QMatmul, QMaximum, QMinimum, QMultiply, QSubtract
+
+__all__ = [
+    "QMeanPow2",
+    "QSum",
+    "QAdd",
+    "QAveragePow2",
+    "QDot",
+    "QEinsum",
+    "QMatmul",
+    "QMaximum",
+    "QMinimum",
+    "QMultiply",
+    "QSubtract",
+]
