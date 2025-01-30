51b5b337c4eaa3fb14d851d1c1eff4ff212dc0ce
Chang Sun <chsun@cern.ch>
2025-01-30 18:14:32 +0000

    fix(bn): name and path override use _internal value

--- src/squark/layers/batch_normalization.py
+++ src/squark/layers/batch_normalization.py
@@ -79,10 +79,11 @@ class QBatchNormalization(QLayerBaseSingleInput, BatchNormalization):
         shape = [1] * len(input_shape)
         shape[self.axis] = input_shape[self.axis]
         self._shape = tuple(shape)
-        self.bn_beta.name = 'bn_beta'
-        self.bn_beta.path = self.bn_beta.path.replace('/beta', '/bn_beta')
-        self.bn_gamma.name = 'bn_gamma'
-        self.bn_gamma.path = self.bn_gamma.path.replace('/gamma', '/bn_gamma')
+
+        self.bn_beta._name = 'bn_beta'
+        self.bn_beta._path = self.bn_beta._path.replace('/beta', '/bn_beta')
+        self.bn_gamma._name = 'bn_gamma'
+        self.bn_gamma._path = self.bn_gamma._path.replace('/gamma', '/bn_gamma')
 
     def _scaler_and_offset(self):
         mean = ops.cast(self.moving_mean, self.dtype)
