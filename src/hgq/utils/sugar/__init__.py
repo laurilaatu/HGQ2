a2c5b3aad9b3b8aaffc866a35ed3a4ac360a4022
Chang Sun <chsun@cern.ch>
2025-05-29 15:42:44 -0700

    feat: add paretofront save callback

--- src/hgq/utils/sugar/__init__.py
+++ src/hgq/utils/sugar/__init__.py
@@ -1,6 +1,7 @@
 from ..dataset import Dataset
 from .beta_scheduler import BetaScheduler, PieceWiseSchedule
 from .ebops import FreeEBOPs
+from .pareto import ParetoFront
 from .pbar import PBar
 
-__all__ = ['BetaScheduler', 'PieceWiseSchedule', 'Dataset', 'FreeEBOPs', 'PBar']
+__all__ = ['BetaScheduler', 'PieceWiseSchedule', 'Dataset', 'FreeEBOPs', 'PBar', 'ParetoFront']
