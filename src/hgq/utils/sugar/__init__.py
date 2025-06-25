f57523171bd594f136cc3bc8684c60f6bf45b2b0
laurilaatu <laurilaatu@users.noreply.github.com>
2025-06-25 20:44:55 +0100

    Add BetaPID (#1)
    * Add BetaPID
    
    * Normalize by target ebops

--- src/hgq/utils/sugar/__init__.py
+++ src/hgq/utils/sugar/__init__.py
@@ -1,7 +1,8 @@
 from ..dataset import Dataset
+from .beta_pid import BetaPID
 from .beta_scheduler import BetaScheduler, PieceWiseSchedule
 from .ebops import FreeEBOPs
 from .pareto import ParetoFront
 from .pbar import PBar
 
-__all__ = ['BetaScheduler', 'PieceWiseSchedule', 'Dataset', 'FreeEBOPs', 'PBar', 'ParetoFront']
+__all__ = ['BetaPID','BetaScheduler', 'PieceWiseSchedule', 'Dataset', 'FreeEBOPs', 'PBar', 'ParetoFront']
