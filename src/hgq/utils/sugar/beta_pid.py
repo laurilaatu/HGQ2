ad0ed16cadcc077126cf1553be4c88ade3166560
Chang Sun <chsun@cern.ch>
2025-06-25 21:01:14 +0000

    fix(sugar/pid): hotfix

--- src/hgq/utils/sugar/beta_pid.py
+++ src/hgq/utils/sugar/beta_pid.py
@@ -1,10 +1,11 @@
-from typing import Optional
+from keras import ops
 from keras.callbacks import Callback
 from keras.models import Model
 
+
 class BetaPID(Callback):
     """
-    Control the beta value of the Q Layers using a PID controller.
+    Control the beta value of the Q-Layers with a PID controller.
 
     Parameters
     ----------
@@ -26,11 +27,6 @@ class BetaPID(Callback):
         If set and the model EBOPs fall below the target EBOPs, the current beta
         will be multiplied by this value. For example, 0.1 will reduce beta by 90%.
         Useful for fine-tuning once the target is reached. If None, no damping is applied.
-
-    Notes
-    -----
-    The beta value is adjusted based on the PID controller output. This helps
-    guide the training process by dynamically controlling the regularization strength.
     """
 
     def __init__(
@@ -42,7 +38,7 @@ class BetaPID(Callback):
         warmup: int = 10,
         init_beta: float = 1e-10,
         max_beta: float = 1e-6,
-        damp_beta_on_target: Optional[float] = None,
+        damp_beta_on_target: float | None = None,
     ) -> None:
         self._beta: float = init_beta
         self.warmup: int = warmup
@@ -52,23 +48,24 @@ class BetaPID(Callback):
         self.target_ebops: float = target_ebops
         self.max_beta: float = max_beta
 
-        self.init_ebops: Optional[float] = None
-        self.damp_beta_on_target: Optional[float] = damp_beta_on_target
+        self.init_ebops: float | None = None
+        self.damp_beta_on_target: float | None = damp_beta_on_target
 
         self.integral: float = 0.0
         self.prev_error: float = 0.0
 
     def get_model_ebops(self) -> float:
+        assert isinstance(self.model, Model)
         ebops: float = 0.0
         for layer in self.model.layers:
-            if hasattr(layer, "ebops"):
+            if hasattr(layer, 'ebops'):
                 ebops += layer.ebops
         return ebops
 
     def update_beta(self) -> float:
         current_value: float = self.get_model_ebops()
 
-        error: float = 1 - self.current_value / self.target_ebops
+        error: float = 1 - current_value / self.target_ebops
         self.integral += error
         derivative: float = error - self.prev_error
 
@@ -77,7 +74,7 @@ class BetaPID(Callback):
 
         return -pid_out
 
-    def on_epoch_begin(self, epoch: int, logs: Optional[dict] = None) -> None:
+    def on_epoch_begin(self, epoch: int, logs: dict | None = None) -> None:
         if epoch == 1:
             if self.init_ebops is None:
                 self.init_ebops = float(self.get_model_ebops())
@@ -88,10 +85,7 @@ class BetaPID(Callback):
             self._beta = max(0.0, new_beta)
             self._beta = min(self._beta, self.max_beta)
 
-            if (
-                self.damp_beta_on_target is not None
-                and self.get_model_ebops() < self.target_ebops
-            ):
+            if self.damp_beta_on_target is not None and self.get_model_ebops() < self.target_ebops:
                 self._beta *= self.damp_beta_on_target
 
             assert isinstance(self.model, Model)
@@ -100,6 +94,6 @@ class BetaPID(Callback):
                 if hasattr(layer, '_beta'):
                     layer._beta.assign(ops.convert_to_tensor(self._beta, dtype=layer._beta.dtype))
 
-    def on_epoch_end(self, epoch: int, logs: Optional[dict] = None) -> None:
+    def on_epoch_end(self, epoch: int, logs: dict | None = None) -> None:
         assert isinstance(logs, dict)
         logs['beta'] = self._beta
